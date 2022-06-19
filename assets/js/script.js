9192f86cf0ce80c2ddba6cb2a16f842c29659a42
Maxim Ivanov <maxim.ivanov@citronium.com>
2022-06-20 00:53:34 +0300

    modify feedback form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
-const openBtn = document.getElementById("open-btn-hero");
+//const openBtnHero = document.getElementById("open-btn-hero");
 const closeBtn = document.getElementById("feedback-close");
 const feedbackForm = document.getElementById("feedback-form");
 
-
-openBtn.addEventListener('click', () => {
-    console.log('dd');
+const buttons = document.querySelectorAll('.modal-btn');
+for(let i = 0 ; i < buttons.length ; i++) {
+  const btn = buttons[i]; 
+  btn.addEventListener('click' , (e) => {
     feedbackForm.classList.add('active');
-})
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+  })
+}
+
+// if (openBtnHero) {
+//     openBtnHero.addEventListener('click', () => {
+//         feedbackForm.classList.add('active');
+//         document.body.style.overflow = "hidden";
+//         document.body.style.height = "100%";
+//     })
+// }
+
 
 closeBtn.addEventListener('click', () => {
     feedbackForm.classList.remove('active')
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
 })
+
